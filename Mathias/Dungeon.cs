25994520d8a453441c7397cf6c89dc4dd184d7d1
Mathias Bevers <bevertje01@outlook.com>
2022-05-19 11:25:44 +0200

    clean room generation code

--- Mathias/Dungeon.cs
+++ Mathias/Dungeon.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using Mathias.Utilities;
+using System.Linq;
 
 namespace Mathias
 {
 	public class Dungeon : DungeonBase
 	{
+		private readonly List<Tuple<Room, Room>> linkedRoomsCollection = new();
 		private int minimumRoomSize;
 
 		public Dungeon(Size pSize) : base(pSize) { }
@@ -30,7 +31,7 @@ namespace Mathias
 
 				Tuple<Room, Room> splitRooms = SplitRoom(splittingRoom);
 
-				if (splitRooms.Item2 == null)
+				if (splitRooms.Item2 == null) // Splitting failed.
 				{
 					unsplitableRooms.Add(splittingRoom);
 					continue;
@@ -38,9 +39,7 @@ namespace Mathias
 
 				rooms.Add(splitRooms.Item1);
 				rooms.Add(splitRooms.Item2);
-
 				rooms.Remove(splittingRoom);
-				unsplitableRooms.Clear();
 			}
 		}
 
@@ -49,45 +48,28 @@ namespace Mathias
 			Room a;
 			Room b;
 
-			bool horizontalSplit;
-
 			if (baseRoom.IsSplitHorizontally)
 			{
 				if (baseRoom.Size.Width - minimumRoomSize < minimumRoomSize) { return new Tuple<Room, Room>(baseRoom, null); }
 
-				horizontalSplit = false;
-			}
-			else
-			{
-				if (baseRoom.Size.Height - minimumRoomSize < minimumRoomSize) { return new Tuple<Room, Room>(baseRoom, null); }
-
-				horizontalSplit = true;
-			}
-
-
-			if (horizontalSplit)
-			{
-				int cutSize = baseRoom.Size.Height - new Random().Next(minimumRoomSize, baseRoom.Size.Height - minimumRoomSize);
-
-				a = new Room(baseRoom.Position.X, baseRoom.Position.Y, baseRoom.Size.Width, cutSize + 1);
-				b = new Room(baseRoom.Position.X, baseRoom.Position.Y + cutSize, baseRoom.Size.Width, baseRoom.Size.Height - cutSize);
-
-				a.IsSplitHorizontally = b.IsSplitHorizontally = horizontalSplit;
-			}
-			else
-			{
 				int cutSize = baseRoom.Size.Width - new Random().Next(minimumRoomSize, baseRoom.Size.Width - minimumRoomSize);
 
 				a = new Room(baseRoom.Position.X, baseRoom.Position.Y, cutSize + 1, baseRoom.Size.Height);
 				b = new Room(baseRoom.Position.X + cutSize, baseRoom.Position.Y, baseRoom.Size.Width - cutSize, baseRoom.Size.Height);
 
-				a.IsSplitHorizontally = b.IsSplitHorizontally = horizontalSplit;
+				a.IsSplitHorizontally = b.IsSplitHorizontally = false;
 			}
+			else
+			{
+				if (baseRoom.Size.Height - minimumRoomSize < minimumRoomSize) { return new Tuple<Room, Room>(baseRoom, null); }
 
+				int cutSize = baseRoom.Size.Height - new Random().Next(minimumRoomSize, baseRoom.Size.Height - minimumRoomSize);
 
-			if (b.Size.Width < minimumRoomSize ||
-			    b.Size.Height < minimumRoomSize) { return new Tuple<Room, Room>(baseRoom, null); }
+				a = new Room(baseRoom.Position.X, baseRoom.Position.Y, baseRoom.Size.Width, cutSize + 1);
+				b = new Room(baseRoom.Position.X, baseRoom.Position.Y + cutSize, baseRoom.Size.Width, baseRoom.Size.Height - cutSize);
 
+				a.IsSplitHorizontally = b.IsSplitHorizontally = true;
+			}
 
 			return new Tuple<Room, Room>(a, b);
 		}
